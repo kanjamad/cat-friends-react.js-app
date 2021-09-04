4b18afb38c0ac869b21f61b5642c2ab40a65b7e0
kanjamad bosel <kanjamadpor@gmail.com>
2021-09-04 05:08:48 -0700

    add toggle button

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,43 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Card.css";
 
-const Card = ({
-  name,
-  email,
-  id,
-  username,
-  phone,
-  website,
-  company,
-  address,
-}) => {
-  return (
-    <div className="card-container">
-      <img
-        alt="robots"
-        src={`https://robohash.org/${id}?set=set4&size=180x180`}
-      />
-      <div>
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showInfo: false,
+    };
+  }
+
+  toggleButton = () => {
+    this.setState({
+      showInfo: !this.state.showInfo,
+    });
+  };
+
+  render() {
+    const { name, email, id, username, phone, website, company, address } =
+      this.props;
+
+    return (
+      <div className="card-container">
+        <img
+          alt="robots"
+          src={`https://robohash.org/${id}?set=set4&size=180x180`}
+        />
         <h2>{name}</h2>
-        <p>username: {username}</p>
         <p>email:{email}</p>
-        <div>
-          <h3>Address</h3>
-          <p>street: {address.street}</p>
-          <p>suite: {address.suite}</p>
-          <p>city: {address.city}</p>
-          <p>zipcode: {address.zipcode}</p>
-        </div>
-        <p>{phone}</p>
-        <p>website:{website}</p>
-        <ul>
-          <li>company : {company.name}</li>
-          <li>catchPhrase : {company.catchPhrase}</li>
-          <li>bs : {company.bs}</li>
-        </ul>
+        <h4>
+          info{" "}
+          <span onClick={this.toggleButton}>
+            <i className="fas fa-caret-square-down"></i>
+          </span>
+        </h4>
+        {this.state.showInfo && (
+          <section>
+            <p>username: {username}</p>
+            <h4>Address</h4>
+            <p>street: {address.street}</p>
+            <p>suite: {address.suite}</p>
+            <p>city: {address.city}</p>
+            <p>zipcode: {address.zipcode}</p>
+            <p>{phone}</p>
+            <p>website:{website}</p>
+            <ul>
+              <li>company : {company.name}</li>
+              <li>catchPhrase : {company.catchPhrase}</li>
+              <li>bs : {company.bs}</li>
+            </ul>
+          </section>
+        )}
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Card;
